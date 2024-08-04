99203dde1d4d4c9e15590567986e8ae33af4ac08
yangseongyeal2 <seongyeolyang7@gmail.com>
2024-08-04 19:17:46 +0900

    add full height

--- src/app/component/TextReveal.tsx
+++ src/app/component/TextReveal.tsx
@@ -10,7 +10,7 @@ const TextReveal: React.FC<TextRevealProps> = ({ text}) => {
     return (
         <div
             className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-500 z-10 min-h-screen">
-            <div className="overflow-hidden text-5xl font-normal leading-normal text-white font-ink-lipquid">
+            <div className="text-5xl font-normal leading-normal text-white font-ink-lipquid">
                 {text.match(/./gu)!.map((char, index) => (
                     <span
                         className="animate-text-reveal inline-block [animation-fill-mode:backwards]"
