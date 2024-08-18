5f663009780d94cfb3539a88193af74c0d79b177
yangseongyeal2 <seongyeolyang7@gmail.com>
2024-08-18 15:14:03 +0900

    데스크 탑 크기 변경 완료

--- src/app/tw/component/TextRevealTW.tsx
+++ src/app/tw/component/TextRevealTW.tsx
@@ -12,7 +12,7 @@ const TextRevealTW: React.FC<TextRevealProps> = ({text}) => {
         <div
             className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-500 z-10 min-h-screen">
             <div className={'flex flex-col items-center justify-center'}>
-                <div className="text-5xl font-normal leading-normal text-white font-MoonTime">
+                <div className="text-5xl md:text-7xl font-normal leading-normal text-white font-MoonTime">
                     {text.match(/./gu)!.map((char, index) => (
                         <span
                             className="animate-text-reveal inline-block [animation-fill-mode:backwards]"
